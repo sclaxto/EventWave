e9a560a79ba8a41bbb8b16bd34ad2e53000f1700
username <youremail@domain.com>
2022-04-29 13:34:02 -0700

    first model

--- eventwave/eventwave/settings.py
+++ eventwave/eventwave/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,11 +76,10 @@ WSGI_APPLICATION = 'eventwave.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'events',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- eventwave/main_app/models.py
+++ eventwave/main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200) # event title
+    seekgeek_id = models.IntegerField() # seekgeek title_id
+    url = models.URLField(max_length=200)
+    pub = models.DateTimeField()
+    performer = models.CharField(max_length=200)
+    kind = models.CharField(max_length=200) # <-- type of event sport, concert ect..
+    
+
+
+
+
+
+
+
+
+
+
