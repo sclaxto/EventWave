3ea1e3675c5db7322ac860937a1684daa02fca68
FrazierMark <m.mark.frazier@gmail.com>
2022-05-01 14:21:37 -0700

    Updated results and views for result

--- eventwave/main_app/views.py
+++ eventwave/main_app/views.py
@@ -29,3 +29,43 @@ def index(request):
     context = dict_of(title, seekgeek_id, url, pub, performer, performers, performerArray, kind, image)
     print(context)
     return render(request, 'events/index.html', {'context': context})
+
+
+def results(request):
+    #     # build query
+    zip = request.GET.get('zip')
+    radius = request.GET.get('radius')
+    type = request.GET.get('type')
+    query = f'{BASE_URL}geoip={zip}&range={radius}mi{PER_PAGE}{CLIENT_ID}'
+
+#     # api call
+    response = requests.get(query)
+    responseData = response.json()
+
+
+#     # compile variables
+    eventsContext = []
+    for event in responseData['events']:
+        print(f'{event} ----- EVENT!!')
+        title = responseData['events'][0]['title']
+
+        seekgeek_id = responseData['events'][0]['id']
+        url = responseData['events'][0]['url']
+        pub = responseData['events'][0]['datetime_utc']
+        kind = responseData['events'][0]['type']
+        image = responseData['events'][0]['performers'][0]['image']
+        performer = responseData['events'][0]['performers'][1]['name']
+        performers = responseData['events'][0]['performers']
+        performerArray = []
+        for performer in performers:
+            performerArray.append(performer['name'])
+
+        context = dict_of(title, seekgeek_id, url, pub, performer,
+                          performers, performerArray, kind, image)
+
+        eventsContext.append(context)
+
+    print(eventsContext)
+    return render(request, 'events/results.html', {'eventsContext': eventsContext})
+
+
