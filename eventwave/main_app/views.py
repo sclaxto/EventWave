a4d8b6d222fe650b932913c8ef691d9f2069cb7f
icefireknight <47699861+icefireknight@users.noreply.github.com>
2022-05-01 12:52:50 -0700

    shows all performers of event

--- eventwave/main_app/views.py
+++ eventwave/main_app/views.py
@@ -8,7 +8,7 @@ from sorcery import dict_of
 
 def index(request):
     response = requests.get(
-        "https://api.seatgeek.com/2/events?geoip=98.213.245.205&per_page=1&client_id=MjAxNTMyNjV8MTY1MTE4OTU5My40NDUzMzAx")
+        "https://api.seatgeek.com/2/events?q=gorillaz&per_page=1&client_id=MjAxNTMyNjV8MTY1MTE4OTU5My40NDUzMzAx")
 
     responseData = response.json()
 
@@ -16,9 +16,16 @@ def index(request):
     seekgeek_id = responseData['events'][0]['id']
     url = responseData['events'][0]['url']
     pub = responseData['events'][0]['datetime_utc']
-    performer = responseData['events'][0]['performers'][0]['name']
+    performer = responseData['events'][0]['performers'][3]['name']
+    performers = responseData['events'][0]['performers']
+    performerArray = []
+    for performer in performers:
+        performerArray.append(performer['name'])
+
+    print(performerArray)
+    # performerArray = []
     kind = responseData['events'][0]['type']
 
-    context = dict_of(title, seekgeek_id, url, pub, performer, kind)
+    context = dict_of(title, seekgeek_id, url, pub, performer, performers, performerArray, kind)
     print(context)
     return render(request, 'events/index.html', {'context': context})
