2650ee194b49b3364ae8cc861fdb35fe45c792ee
FrazierMark <m.mark.frazier@gmail.com>
2022-05-01 15:55:06 -0700

    Results page now populating

--- eventwave/main_app/views.py
+++ eventwave/main_app/views.py
@@ -4,6 +4,9 @@ import requests
 from sorcery import dict_of
 
 # Create your views here.
+BASE_URL = 'https://api.seatgeek.com/2/events?'
+PER_PAGE = '&per_page=20'
+CLIENT_ID = "&client_id=MjAxNTMyNjV8MTY1MTE4OTU5My40NDUzMzAx"
 
 
 def index(request):
@@ -22,12 +25,11 @@ def index(request):
     for performer in performers:
         performerArray.append(performer['name'])
 
-    print(performerArray)
     kind = responseData['events'][0]['type']
     image = responseData['events'][0]['performers'][0]['image']
 
-    context = dict_of(title, seekgeek_id, url, pub, performer, performers, performerArray, kind, image)
-    print(context)
+    context = dict_of(title, seekgeek_id, url, pub, performer,
+                      performers, performerArray, kind, image)
     return render(request, 'events/index.html', {'context': context})
 
 
@@ -36,6 +38,7 @@ def results(request):
     zip = request.GET.get('zip')
     radius = request.GET.get('radius')
     type = request.GET.get('type')
+
     query = f'{BASE_URL}geoip={zip}&range={radius}mi{PER_PAGE}{CLIENT_ID}'
 
 #     # api call
@@ -46,16 +49,15 @@ def results(request):
 #     # compile variables
     eventsContext = []
     for event in responseData['events']:
-        print(f'{event} ----- EVENT!!')
-        title = responseData['events'][0]['title']
 
-        seekgeek_id = responseData['events'][0]['id']
-        url = responseData['events'][0]['url']
-        pub = responseData['events'][0]['datetime_utc']
-        kind = responseData['events'][0]['type']
-        image = responseData['events'][0]['performers'][0]['image']
-        performer = responseData['events'][0]['performers'][1]['name']
-        performers = responseData['events'][0]['performers']
+        title = event['title']
+        seekgeek_id = event['id']
+        url = event['url']
+        pub = event['datetime_utc']
+        kind = event['type']
+        image = event['performers'][0]['image']
+        # performer = event['performers'][1]['name']
+        performers = event['performers']
         performerArray = []
         for performer in performers:
             performerArray.append(performer['name'])
@@ -64,8 +66,4 @@ def results(request):
                           performers, performerArray, kind, image)
 
         eventsContext.append(context)
-
-    print(eventsContext)
     return render(request, 'events/results.html', {'eventsContext': eventsContext})
-
-
